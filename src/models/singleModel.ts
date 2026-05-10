d4e04b82574839833cdf48de876a4b270adf880c
Gabriel Sanhueza <gabriel_8032@hotmail.com>
2026-05-10 03:15:19 -0400

    fix: fixed context size detection for single mode

--- src/models/singleModel.ts
+++ src/models/singleModel.ts
@@ -1,8 +1,23 @@
+import { DEFAULT_CTX } from "../constants";
 import { Mode } from "../enums/mode";
+import { PropsEndpoint } from "../interfaces/endpoints/props";
+import { rpc } from "../tools/retriever";
 import { BaseModel } from "./baseModel";
 
 export class SingleModel extends BaseModel {
   get mode(): Mode {
     return Mode.SINGLE;
+  }
+
+  async getContextSize(): Promise<number> {
+    try {
+      const { default_generation_settings } = await rpc<PropsEndpoint>(
+        `/props?model=${this.id}`,
+      );
+      const { n_ctx } = default_generation_settings;
+      return n_ctx;
+    } catch {
+      return DEFAULT_CTX;
+    }
   }
 }
